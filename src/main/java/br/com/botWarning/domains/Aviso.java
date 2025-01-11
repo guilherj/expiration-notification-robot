d88c964d079d4d8e6648015c6ff7c92c4fc03d97
guilherj <contato.guilhermenascimento@hotmail.com>
2025-01-11 20:49:38 -0300

    refactor :recycle: Refactoring class attributes

--- src/main/java/br/com/botWarning/domains/Aviso.java
+++ src/main/java/br/com/botWarning/domains/Aviso.java
@@ -6,8 +6,11 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import br.com.botWarning.domains.enums.QuandoAvisar;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,15 +30,14 @@ public class Aviso implements Serializable {
 	@Id
 	@Getter
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
+	private Long id;	
 	@Getter @Setter
 	private String mensagem;
 	@Getter @Setter
-	private Integer quandoAvisar;
+	@Enumerated(EnumType.STRING)
+	private QuandoAvisar quandoAvisar;
 	@Getter @Setter
-	private Integer diasAntes;
-	
+	private Integer diasAntes;	
 	@Getter @Setter
 	@Column(nullable = false)
 	private Boolean reenviar;
